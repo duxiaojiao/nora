6d702c1516ab569f97ab20e8431cc97393c13b61
Taylor <j_long@ctrip.com>
2019-03-26 00:23:53 +0800

    登录页面更新

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,15 +1,61 @@
 import React, { Component } from 'react';
+import { Button, Row, Form, Icon, Input } from 'antd'
 
 
+const FormItem = Form.Item
 class Index extends Component {
 
 
   render() {
-    return (<div>
-      <a>测试登录</a>
-    </div>);
+    const { loading, form} = this.props
+    const { getFieldDecorator } = form
+
+    return (
+      <div>
+        <form>
+          <FormItem hasFeedback>
+            {getFieldDecorator('username', {
+              rules: [
+                {
+                  required: true,
+                },
+              ],
+            })(
+              <Input
+                onPressEnter={this.handleOk}
+                placeholder='用户名'
+              />
+            )}
+          </FormItem>
+          <FormItem hasFeedback>
+            {getFieldDecorator('password', {
+              rules: [
+                {
+                  required: true,
+                },
+              ],
+            })(
+              <Input
+                type="password"
+                onPressEnter={this.handleOk}
+                placeholder='密码'
+              />
+            )}
+          </FormItem>
+          <Row>
+            <Button
+              type="primary"
+              onClick={this.handleOk}
+              // loading={loading.effects.login}
+            >
+
+            </Button>
+          </Row>
+        </form>
+    </div>
+    );
   }
 
 }
 
-export default Index;
+export default Form.create()(Index);;
